ff8f41ca8d71180914043e797075a282f6696445
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-17 22:52:23 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+prefix = '!'
+mp3_dir = 'music'
+pic_dir = 'co_memes'
+res_dir = 'resources'
+temp_mp3_name = 'song.mp3'
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
