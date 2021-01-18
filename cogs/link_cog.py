a30b06707fdf9d160a4cc62190e18cb0b6d3f413
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-18 21:31:39 +0100

    Update link_cog.py

--- cogs/link_cog.py
+++ cogs/link_cog.py
@@ -21,7 +21,13 @@ class LinkCog(commands.Cog):
         await build_link_list(ctx, embed_var, "oflinki")
 
     @commands.command()
-    async def link(self, ctx, *, subject):
+    async def link(self, ctx, *, subject_alias):
+        subject = ""
+        with open(f"{res_dir}/subject_aliases.txt", "r") as rd:
+            subject_alias_dict = json.loads('{' + rd.read() + '}')
+        for key in subject_alias_dict:
+            if subject_alias in subject_alias_dict[key] or subject_alias == key:
+                subject = key
         help_json = "".join(read_lines(f'{res_dir}/linki'))
         embed_var = discord.Embed(title=subject, description=json.loads('{' + help_json + '}')[subject], color=0xff770f)
         await ctx.send(embed=embed_var)
