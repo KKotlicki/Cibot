c27ac208275a810b35fe9b204231fc113fddf081
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-18 12:39:26 +0100

    Create fun_cog.py

---
+++ cogs/fun_cog.py
@@ -0,0 +1,19 @@
+from discord.ext import commands
+from config import *
+from helpers import get_random_number_unless_specified, send_pic_or_txt_on_choice, read_lines
+
+
+class Fun(commands.Cog):
+    co_alias = read_lines(f'{res_dir}/co_aliases')
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=co_alias)
+    async def co(self, ctx, *, question=''):
+        choice = get_random_number_unless_specified(question)
+        await send_pic_or_txt_on_choice(ctx, choice)
+
+
+def setup(client):
+    client.add_cog(Fun(client))
