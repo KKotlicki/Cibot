8975ab0bd6b42f2c8cad80e394f1751baab84d95
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-18 14:13:43 +0100

    Update cibot.py

--- cibot.py
+++ cibot.py
@@ -2,16 +2,13 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from config import *
-
 load_dotenv()
 
-# Set command prefix:
-client = commands.Bot(command_prefix=prefix)
 
+client = commands.Bot(command_prefix=prefix)
 if __name__ == '__main__':
     for filename in os.listdir(f'{cogs_dir}'):
         if filename.endswith('.py'):
             client.load_extension(f'{cogs_dir}.{filename[:-3]}')
-
 
 client.run(os.getenv("DSC_BOT_KEY"))
