621ed285083b277110540531ca20fc5e36a03e13
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-18 07:28:35 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,14 +2,32 @@ import random
 import discord
 from config import *
 import glob
-
+import json
 
 def read_file(file_name):
     with open(f"{file_name}.txt") as rs:
         return rs.read().splitlines()
 
 
+async def build_link_list(ctx, embed_var, fname):
+    help_json = "".join(read_file(f'{res_dir}/{fname}'))
+    for name, value in json.loads('{' + help_json + '}').items():
+        embed_var.add_field(name=f'**{name}**', value=value, inline=False)
+    await ctx.send(embed=embed_var)
+
+
+async def fetch_sv_data(guild):
+    text_names = []
+    voice_names = []
+    for channel in guild.text_channels:
+        text_names.append(str(channel))
+    for channel in guild.voice_channels:
+        voice_names.append(str(channel))
+    with open(f"{sv_dir}/{guild}.txt", "w+") as fn:
+        fn.write(f'"text": {text_names}, "voice": {voice_names}')
+
 # Local functions:
+
 
 def get_random_number_unless_specified(question):
     if question == '1':
