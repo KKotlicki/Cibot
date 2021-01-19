fe3203df75f68ea586c7c8e3609deeb31e91f80b
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-19 23:06:57 +0100

    Update admin_cog.py

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -23,6 +23,7 @@ class AdminCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def update(self, ctx):
         os.system('/home/pi/bot/cibot/updater.sh')
+
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def adm(self, ctx):
@@ -31,7 +32,8 @@ class AdminCog(commands.Cog):
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def shutdown(self, ctx):
-        await self.client.close()
+        await self.bot.close()
+
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=5):
