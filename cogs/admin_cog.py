6220fe20068ee0f8595b7979359adf1488d67819
frytki sa smaczne <frytkisasmaczne@gmail.com>
2021-01-19 23:06:49 +0100

    client->bot

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -31,7 +31,7 @@ class AdminCog(commands.Cog):
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def shutdown(self, ctx):
-        await self.client.close()
+        await self.bot.close()
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
     async def clear(self, ctx, amount=5):
