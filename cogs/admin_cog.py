216a5bb96f7fa84320bfbcdd28f6680a6ac0c42b
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 06:35:31 +0100

    Update admin_cog.py

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -3,27 +3,12 @@ from discord.ext import commands
 from helpers import fetch_sv_data, open_help
 from config import sv_dir
 import json
-from loguru import logger
-import os
 
 
 class AdminCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.message_channel = ""
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        if os.path.exists("update.sh"):
-            os.remove("update.sh")
-        if os.path.exists("update.bat"):
-            os.remove("update.bat")
-        print(f"Logged in as {self.bot.user}")
-        logger.info(f"Logged in as {self.bot.user}")
-
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        await fetch_sv_data(guild)  
     
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
