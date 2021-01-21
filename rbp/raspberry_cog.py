5755b0ccee59fd450fc30212566da0f2a43645e8
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 15:23:23 +0100

    Update raspberry_cog.py

--- rbp/raspberry_cog.py
+++ rbp/raspberry_cog.py
@@ -17,6 +17,7 @@ class RaspberryCog(commands.Cog):
             temp = os.popen("vcgencmd measure_temp").readline()
             if float(temp.replace("temp=", "").replace("'C", "")) > temperature_limit:
                 logger.exception("Server overheated.")
+                logger.add('dumps/errors.log', rotation="10 MB")
                 await self.bot.close()
 
     @commands.command(pass_context=True)
