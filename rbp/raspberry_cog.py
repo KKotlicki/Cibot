bb80594d7d0b177b1d4dd9735f636abd8333dd37
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 08:40:19 +0100

    Update raspberry_cog.py

--- rbp/raspberry_cog.py
+++ rbp/raspberry_cog.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands, tasks
 import os
 
@@ -5,13 +6,15 @@ import os
 class RaspberryCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.raspberry_temp.start()
 
     @tasks.loop(minutes=5.0)
-    async def rasp_temp(self):
-        temp = os.popen("vcgencmd measure_temp").readline()
-        print(temp)
-        if float(temp.replace("temp=", "").replace("'C", "")) > 59.0:
-            await self.bot.close()
+    async def raspberry_temp(self):
+        if discord.Client.is_ready(self.bot):
+            temp = os.popen("vcgencmd measure_temp").readline()
+            print(temp)
+            if float(temp.replace("temp=", "").replace("'C", "")) > 59.0:
+                await self.bot.close()
 
 
 def setup(bot):
