1fe3a9a8b144f326869141643bf86d6d0bd73aa2
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 19:17:12 +0100

    Update yt_cog.py

--- cogs/yt_cog.py
+++ cogs/yt_cog.py
@@ -26,7 +26,8 @@ class YtLinkAudioCog(commands.Cog):
                     ":slight_frown: Zaczekaj aż skończy się aktualny utwór, lub zakończ go komendą \"stop\".")
                 return
             await ctx.send(":satellite: Buforuję...")
-            await self.bot.change_presence(status=discord.Status.online, activity=discord.Game('podłsuchuje telekomune'))
+            await self.bot.change_presence(
+                status=discord.Status.online, activity=discord.Game('podłsuchuje telekomune'))
             await self.download_and_play_video(ctx, channel, url)
 
     @commands.command()
