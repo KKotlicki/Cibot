8f9e44cb3658bc0f1313528ca4eee4ecbcfff713
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 22:34:48 +0100

    Update link_cog.py

--- cogs/link_cog.py
+++ cogs/link_cog.py
@@ -1,6 +1,8 @@
 import discord
+import json
 from discord.ext import commands
 from helpers import build_link_list
+from config import res_dir
 
 
 class LinkCog(commands.Cog):
@@ -18,18 +20,26 @@ class LinkCog(commands.Cog):
         embed_var = discord.Embed(title=":mortar_board: Oficjalne linki:", color=0xff770f)
         await build_link_list(ctx, embed_var, "oflinki", message)
 
-    # @commands.command()
-    # async def link(self, ctx, *, comb):
-    #     subject = ""
-    #     with open(f"{res_dir}/subject_aliases.txt", "r") as rd:
-    #         subject_alias_dict = json.loads('{' + rd.read() + '}')
-    #     for key in subject_alias_dict:
-    #         if subject_alias in subject_alias_dict[key] or subject_alias == key:
-    #             subject = key
-    #             break
-    #     help_json = "".join(read_lines(f'{res_dir}/linki'))
-    #     embed_var = discord.Embed(title=subject, description=json.loads('{' + help_json + '}')[subject], color=0xff770f)
-    #     await ctx.send(embed=embed_var)
+    @commands.command()
+    async def link(self, ctx, *, comb):
+        print(comb)
+        subject = ""
+        if len(comb.split(" ")) < 2:
+            subject_alias = "".join(comb.split(" ")[1:])
+            print(subject_alias)
+            sem = comb.split(" ")[0]
+            print(sem)
+            with open(f"{res_dir}/subject_aliases.txt", "r") as rd:
+                subject_alias_dict = json.loads(rd.read())[sem]
+            for key in subject_alias_dict:
+                if subject_alias in subject_alias_dict[key] or subject_alias == key:
+                    subject = key
+                    break
+            print(subject_alias_dict[sem][subject])
+            embed_var = discord.Embed(title=subject, description=subject_alias_dict[sem][subject], color=0xff770f)
+            await ctx.send(embed=embed_var)
+        else:
+            ctx.send("Za mało argumentów. Podaj komendę w postaci: !link <nr. semestru> <przedmiot>")
 
 
 def setup(bot):
