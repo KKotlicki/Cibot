a5b03ccb40093c538c11eb604768d5f4b0d9a8a6
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 08:08:32 +0100

    Update raspberry_cog.py

--- rbp/raspberry_cog.py
+++ rbp/raspberry_cog.py
@@ -1,24 +1,17 @@
-import discord
-from discord.ext import commands
-from helpers import fetch_sv_data, open_help
-from config import *
-import json
-from loguru import logger
+from discord.ext import commands, tasks
 import os
 
 
 class RaspberryCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        print("f")
 
-    # @commands.Cog.listener()
-    # async def on_ready(self):
-    #
-    #
-    # @commands.Cog.listener()
-    # async def on_guild_join(self, guild):
-    #     await fetch_sv_data(guild)
+    @tasks.loop(minutes=5.0)
+    async def rasp_temp(self):
+        temp = os.popen("vcgencmd measure_temp").readline()
+        print(temp)
+        if float(temp.replace("temp=", "").replace("'C", "")) > 59.0:
+            await self.bot.close()
 
 
 def setup(bot):
