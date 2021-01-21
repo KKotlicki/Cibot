90ea75502e89698540fe07dafcd4913e8e651221
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 09:31:10 +0100

    Update raspberry_cog.py

--- rbp/raspberry_cog.py
+++ rbp/raspberry_cog.py
@@ -2,19 +2,21 @@ import discord
 from discord.ext import commands, tasks
 import os
 from loguru import logger
+from .rasp_config import *
 
 
 class RaspberryCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.raspberry_temp.start()
+        if temperature_limit is not None:
+            self.raspberry_temp.start()
 
     @tasks.loop(minutes=5.0)
     async def raspberry_temp(self):
         if discord.Client.is_ready(self.bot):
             temp = os.popen("vcgencmd measure_temp").readline()
             print(temp)
-            if float(temp.replace("temp=", "").replace("'C", "")) > 59.0:
+            if float(temp.replace("temp=", "").replace("'C", "")) > temperature_limit:
                 logger.exception("Server overheated.")
                 await self.bot.close()
 
