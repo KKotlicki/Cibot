20c717c86566d19083c7b5b1ae0643f4947561c1
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 09:15:14 +0100

    Update utility_cog.py

--- cogs/utility_cog.py
+++ cogs/utility_cog.py
@@ -18,11 +18,21 @@ class UtilityCog(commands.Cog):
             os.remove("update.bat")
         print(f"Logged in as {self.bot.user}")
         logger.info(f"Logged in as {self.bot.user}")
+        if not os.path.exists('dumps/errors.log'):
+            logger.add('dumps/errors.log', rotation="10 MB")
         await self.bot.change_presence(status=discord.Status.idle, activity=discord.Game('udaje studenta...'))
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         await fetch_sv_data(guild)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, err):
+        if isinstance(err, commands.CommandNotFound):
+            logger.exception("Invalid command used.")
+            await ctx.send("Invalid command used.")
+        else:
+            logger.error(err)
 
 
 def setup(bot):
