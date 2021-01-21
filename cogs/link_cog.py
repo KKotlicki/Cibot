41da654bf7892774abdc5f93cf54d6c23a4f96d3
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 21:08:58 +0100

    Update link_cog.py

--- cogs/link_cog.py
+++ cogs/link_cog.py
@@ -10,28 +10,30 @@ class LinkCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def linki(self, ctx):
-        embed_var = discord.Embed(title=":shushing_face: Linki pochodzą z:", description="https://tiny.cc/szukamlinku",
+    async def linki(self, ctx, message):
+        embed_var = discord.Embed(title=":shushing_face: Nieoficjalne linki:", description="semestr "+message,
                                   color=0xff770f)
-        await build_link_list(ctx, embed_var, "linki")
+        with open(f'{res_dir}/linki.json', 'r') as rd:
+            linki_dict = json.loads(rd.read())[message]
+        await build_link_list(ctx, embed_var, linki_dict)
 
     @commands.command()
     async def oflinki(self, ctx):
         embed_var = discord.Embed(title=":mortar_board: Oficjalne linki:", color=0xff770f)
         await build_link_list(ctx, embed_var, "oflinki")
 
-    @commands.command()
-    async def link(self, ctx, *, subject_alias):
-        subject = ""
-        with open(f"{res_dir}/subject_aliases.txt", "r") as rd:
-            subject_alias_dict = json.loads('{' + rd.read() + '}')
-        for key in subject_alias_dict:
-            if subject_alias in subject_alias_dict[key] or subject_alias == key:
-                subject = key
-                break
-        help_json = "".join(read_lines(f'{res_dir}/linki'))
-        embed_var = discord.Embed(title=subject, description=json.loads('{' + help_json + '}')[subject], color=0xff770f)
-        await ctx.send(embed=embed_var)
+    # @commands.command()
+    # async def link(self, ctx, *, comb):
+    #     subject = ""
+    #     with open(f"{res_dir}/subject_aliases.txt", "r") as rd:
+    #         subject_alias_dict = json.loads('{' + rd.read() + '}')
+    #     for key in subject_alias_dict:
+    #         if subject_alias in subject_alias_dict[key] or subject_alias == key:
+    #             subject = key
+    #             break
+    #     help_json = "".join(read_lines(f'{res_dir}/linki'))
+    #     embed_var = discord.Embed(title=subject, description=json.loads('{' + help_json + '}')[subject], color=0xff770f)
+    #     await ctx.send(embed=embed_var)
 
 
 def setup(bot):
