73b63229d4ab3b8838f26aabda04e2e0d7fe4d5f
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-21 08:40:16 +0100

    Update utility_cog.py

--- cogs/utility_cog.py
+++ cogs/utility_cog.py
@@ -10,16 +10,15 @@ class UtilityCog(commands.Cog):
         self.bot = bot
         self.message_channel = ""
 
-    @commands.Cog.listener(pass_context=True)
-    async def on_ready(self, ctx):
+    @commands.Cog.listener()
+    async def on_ready(self):
         if os.path.exists("update.sh"):
             os.remove("update.sh")
         if os.path.exists("update.bat"):
             os.remove("update.bat")
         print(f"Logged in as {self.bot.user}")
         logger.info(f"Logged in as {self.bot.user}")
-        await ctx.invoke(self.bot.rasp_temp())
-        await self.bot.change_presence(status=discord.Status.idle, activity=discord.Game('Infiltruje studentów...'))
+        await self.bot.change_presence(status=discord.Status.idle, activity=discord.Game('udaje studenta...'))
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
