574de94db2d493b1bd672caca3932a2bd372bea9
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-22 20:50:06 +0100

    Update admin_cog.py

--- cogs/admin_cog.py
+++ cogs/admin_cog.py
@@ -19,7 +19,7 @@ class AdminCog(commands.Cog):
     @commands.has_permissions(administrator=True)
     async def shutdown(self, ctx):
         print("tak")
-        await self.bot.close()
+        self.bot.close()
 
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
