dcefd9724f517c0a96e5401ad585aa5755315276
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-22 23:57:40 +0100

    server down restart

--- rbp/raspberry_cog.py
+++ rbp/raspberry_cog.py
@@ -18,7 +18,8 @@ class RaspberryCog(commands.Cog):
             if float(temp.replace("temp=", "").replace("'C", "")) > temperature_limit:
                 logger.exception("Server overheated.")
                 logger.add('dumps/errors.log', rotation="10 MB")
-                self.bot.close()
+                await self.bot.close()
+                await self.bot.close()
 
     @commands.command(pass_context=True)
     @commands.has_permissions(administrator=True)
--- utility/server_down.py
+++ utility/server_down.py
@@ -7,6 +7,7 @@ load_dotenv()
 while True:
     time.sleep(60)
     try:
+        print('retrying connection')
         urlopen('http://216.58.192.142', timeout=20)
     finally:
         os.system(f'cd ..\n{os.getenv("OS_PYTHON_PREFIX")} cibot.py\n')
