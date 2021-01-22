97b451af2bb3be34c9a21783510c7b94f3661372
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-01-22 01:01:55 +0100

    fixed ai resources collector

--- cogs/ai_cog.py
+++ cogs/ai_cog.py
@@ -6,15 +6,16 @@ class AICog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-
     @commands.Cog.listener()
     async def on_message(self, message):
         for receptor in ai_receptors:
             if receptor in message.content:
-                with open(f"{ai_dir}/ai_data.txt", "r") as rd:
-                    temp = rd.read()
-                with open(f"{ai_dir}/ai_data.txt", "w+") as wr:
-                    wr.write(f"{temp}\n => \n{message.content}")
+                try:
+                    with open(f"{ai_dir}/ai_data.txt", "x"):
+                        wr.write(f"{message.content}")
+                except FileExistsError:
+                    with open(f"{ai_dir}/ai_data.txt", "a") as wr:
+                        wr.write(f"\n{message.content}")
 
 
 def setup(bot):
