1bb7dfa00683b278517abac611fdd1a779cfca00
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-04-09 00:39:16 +0200

    automatic updates

--- cibot.py
+++ cibot.py
@@ -1,42 +1,14 @@
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import os
 import os.path
-from config import *
+from config import prefix, cogs_dir, rasp_dir
 from helpers import env_config
-from platform import system
-import json
 
 
 if not os.path.exists('.env'):
     env_config()
 load_dotenv()
-
-
 bot = commands.Bot(command_prefix=prefix)
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def update(ctx):
-    await ctx.channel.purge(limit=1)
-    with open(f'{res_dir}/status.json', encoding='utf-8') as rd:
-        statuses = json.loads(rd.read())
-    await bot.change_presence(status=discord.Status.dnd, activity=discord.Game(statuses['update']))
-
-    py_prefix = os.getenv("OS_PYTHON_PREFIX")
-    os_system = system()
-
-    if os_system == 'Windows':
-        with open(f'{res_dir}/update_ms.txt', 'r') as rd:
-            updater = rd.read().replace('<python>', py_prefix).replace('<path>', os.getcwd())
-        with open(os.getcwd()+'/update.bat', 'w') as wr:
-            wr.write(updater)
-        os.system('update.bat')
-
-    elif os_system == 'Linux':
-        os.system(f"sudo pkill '{py_prefix} cibot.py'\ngit pull\n{py_prefix} cibot.py\n")
 
 
 if __name__ == '__main__':
--- cogs/utility_cog.py
+++ cogs/utility_cog.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime
 from discord.ext import commands, tasks
 from helpers import fetch_sv_data
 from loguru import logger
@@ -7,6 +8,7 @@ import os
 from urllib.request import urlopen
 from urllib.error import URLError
 from dotenv import load_dotenv
+from platform import system
 import json
 load_dotenv()
 
@@ -26,13 +28,14 @@ class UtilityCog(commands.Cog):
         if not os.path.exists(f'{logs_dir}/errors.log'):
             logger.add(f'{logs_dir}/errors.log', rotation="10 MB")
         self.connection_timeout.start()
+        self.automatic_update.start()
         with open(f'{res_dir}/status.json', encoding='utf-8') as rd:
             statuses = json.loads(rd.read())
         await self.bot.change_presence(status=discord.Status.idle, activity=discord.Game(statuses['active']))
 
     @commands.Cog.listener()
-    async def on_guild_join(self, ctx, guild):
-        await fetch_sv_data(ctx, guild)
+    async def on_guild_join(self, ctx):
+        await fetch_sv_data(ctx)
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, err):
@@ -43,7 +46,7 @@ class UtilityCog(commands.Cog):
         else:
             logger.error(err)
 
-    @tasks.loop(minutes=1.0)
+    @tasks.loop(minutes=2.0)
     async def connection_timeout(self):
         try:
             urlopen('http://216.58.192.142', timeout=20)
@@ -53,6 +56,40 @@ class UtilityCog(commands.Cog):
             os.system(f'cd utility\n{os.getenv("OS_PYTHON_PREFIX")} server_down.py\n')
             print('shutting down')
             await self.bot.close()
+
+    @tasks.loop(minutes=1.0)
+    async def automatic_update(self):
+        now = datetime.now()
+        if now.hour == 4 and not now.minute:
+            ''':)'''
+            await self.bot.change_presence(status=discord.Status.dnd,
+                                           activity=discord.Game("follow the white rabbit :rabbit2:"))
+            if now.minute == 20:
+                await self.update_fun()
+
+    @commands.command(pass_context=True)
+    @commands.has_permissions(administrator=True)
+    async def update(self, ctx):
+        await ctx.channel.purge(limit=1)
+        await self.update_fun()
+
+    async def update_fun(self):
+        with open(f'{res_dir}/status.json', encoding='utf-8') as rd:
+            statuses = json.loads(rd.read())
+        await self.bot.change_presence(status=discord.Status.dnd, activity=discord.Game(statuses['update']))
+
+        py_prefix = os.getenv("OS_PYTHON_PREFIX")
+        os_system = system()
+
+        if os_system == 'Windows':
+            with open(f'{res_dir}/update_ms.txt', 'r') as rd:
+                updater = rd.read().replace('<python>', py_prefix).replace('<path>', os.getcwd())
+            with open(os.getcwd() + '/update.bat', 'w') as wr:
+                wr.write(updater)
+            os.system('update.bat')
+
+        elif os_system == 'Linux':
+            os.system(f"sudo pkill '{py_prefix} cibot.py'\ngit pull\n{py_prefix} cibot.py\n")
 
 
 def setup(bot):
