38437ee9cbdab1d7ed5136e8c0f98f056d224b23
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-04-08 23:43:22 +0200

    fix
    roles no longer duplicates role selection message

--- cogs/main_cog.py
+++ cogs/main_cog.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
-from helpers import open_help
-from config import res_dir, logs_dir
+from helpers import open_help, get_text_channel_id_from_name
+from config import res_dir, logs_dir, sv_dir, prefix
 import json
+import os
 from loguru import logger
 
 
@@ -36,6 +37,64 @@ class MainCog(commands.Cog):
         await ctx.channel.purge(limit=1)
         logger.info("\n<" + str(ctx.author) + "> said:\n<" + message + ">")
         logger.add(f'{logs_dir}/bugs.log', rotation="5 MB")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        for file in os.listdir(f"{sv_dir}/"):
+            if file.endswith("_config.json"):
+                with open(f'{sv_dir}/{file}', encoding='utf-8') as rd:
+                    channels_dict = json.loads(rd.read())
+                if 'role' in channels_dict:
+                    message_channel = channels_dict['role']
+                    try:
+                        channel_id = get_text_channel_id_from_name(file[:-12], message_channel)
+                        channel = self.bot.get_channel(channel_id)
+                        await channel.purge(limit=1)
+                        with open(f'{res_dir}/roles.json', encoding='utf-8') as rd:
+                            role_json = json.loads(rd.read())
+                        roles_list = ""
+                        for key, value in role_json.items():
+                            roles_list += f"\n\n{value}: {key}"
+                        text = f"**Wybór Roli: Grupy**\nZareaguj aby wybrać grupę:{roles_list}"
+                        message = await channel.send(text)
+                        for key, value in role_json.items():
+                            await message.add_reaction(value)
+                    except:
+                        print(f'No saved data or wrong data about server\'s text channels.\n'
+                              f'On all servers using qutomatic roles type in {prefix}sv and set role channel again.\n'
+                              f'Make sure not to misspell the name of the role channel.')
+                        pass
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if not user.bot:
+            channels_list = []
+            for file in os.listdir(f"{sv_dir}/"):
+                if file.endswith("_config.json"):
+                    with open(f'{sv_dir}/{file}', encoding='utf-8') as rd:
+                        channels_dict = json.loads(rd.read())
+                    if 'role' in channels_dict:
+                        message_channel = channels_dict['role']
+                        try:
+                            channel_id = get_text_channel_id_from_name(file[:-12], message_channel)
+                            channel = self.bot.get_channel(channel_id)
+                            channels_list.append(channel.id)
+                        except:
+                            print(f'No saved data or wrong data about server\'s text channels.\n'
+                                  f'On all servers using qutomatic roles type in '
+                                  f'{prefix}sv and set role channel again.\n'
+                                  f'Make sure not to misspell the name of the role channel.')
+                            pass
+            with open(f'{res_dir}/roles.json', encoding='utf-8') as rd:
+                roles_json = json.loads(rd.read())
+            for key, value in roles_json.items():
+                if reaction.message.channel.id in channels_list and reaction.emoji == value:
+                    try:
+                        role = discord.utils.get(reaction.message.guild.roles, name=key)
+                        await user.add_roles(role)
+                    except AttributeError:
+                        print("Server doesn't have this role configured.")
+                    return
 
 
 def setup(bot):
