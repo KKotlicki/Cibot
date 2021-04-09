8be07e51da1710735d020b0f579b21309f39862b
Konrad Kotlicki <konrad.kotlicki@gmail.com>
2021-04-09 13:49:47 +0200

    added random bot reactions

--- cogs/fun_cog.py
+++ cogs/fun_cog.py
@@ -1,3 +1,4 @@
+import random
 from discord.ext import commands
 from config import *
 from helpers import get_random_number_unless_specified, send_pic_or_txt_on_choice, read_lines
@@ -14,7 +15,37 @@ class Fun(commands.Cog):
         choice = get_random_number_unless_specified(question)
         await send_pic_or_txt_on_choice(ctx, choice)
 
-
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if random.randint(1, 16) == 1 and not message.author.bot and not message.content.startswith(prefix):
+            emoji_dict = {
+                1: "👍",
+                2: "❤",
+                3: "🙂",
+                4: "🤯",
+                5: "👀",
+                6: "🧠",
+                7: "🫀",
+                8: "🧐",
+                9: "🥳",
+                10: "😀",
+                11: "😮",
+                12: "🎓",
+                13: "⭐",
+                14: "☢",
+                15: "☣",
+                16: "🧡",
+                17: "💛",
+                18: "💚",
+                19: "💙",
+                20: "💜",
+                21: "🖤",
+                22: "🤍",
+                23: "😃",
+                24: "😄",
+                25: "😲"
+            }
+            await message.add_reaction(emoji_dict[random.randint(1, 16)])
 
 
 def setup(bot):
